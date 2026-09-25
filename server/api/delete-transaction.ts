1de8ff99a7f872b7788726e5672c7fbd2338c94f
jasminework2023 <jasmine.work2023@gmail.com>
2026-09-25 08:55:11 +0700

    Update delete-transaction.ts

--- server/api/delete-transaction.ts
+++ server/api/delete-transaction.ts
@@ -24,7 +24,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const sb = createClient(
       process.env.SUPABASE_URL as string,
-      process.env.SUPABASE_ANON_KEY as string,
+      process.env.SUPABASE_SERVICE_ROLE_KEY as string,
     );
     const { data: u, error } = await sb
       .from("users")
